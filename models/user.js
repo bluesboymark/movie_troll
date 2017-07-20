152ac65c7174a75c7a1dcba458293c321ab171e8
nicholas anggiansah <Mark@Hadys-MacBook-Pro.local>
2017-07-20 14:08:58 -0700

    facebook stuff

--- models/user.js
+++ models/user.js
@@ -3,8 +3,16 @@ const
   passportLocalMongoose = require('passport-local-mongoose')
 
   const UserSchema = new mongoose.Schema({
+    local : {
     username: String,
     password: String
+  },
+  facebook: {
+		id: String,
+		name: String,
+		token: String,
+		email: String
+	}
   });
 
  // PLM has built in methods to encode decode
